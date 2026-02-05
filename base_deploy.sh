b7a3546c165bb3d3a727654802ef41d66597ee2c
min.fang <min.fang@chivox.com>
2026-02-05 23:30:57 +0800

    fix

---
+++ base_deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Build the project.
+hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+# Go To Public folder
+# cd docs
+# Add changes to git.
+rm -rf docs
+mv public docs
